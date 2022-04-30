54f83a9c6f5bd9657f5f90b6b0ec3c8efc7114a0
devcsb <devcsb119@gmail.com>
2022-04-30 20:19:40 +0900

    기본 키 매핑
    @GeneratedValue 전략에 대한 이해,
    권장하는 식별자 전략 숙지,
    IDENTITY 전략의 내부 작동 방식 이해,
    SEQUENCE 전략의 내부 작동 방식 이해(allocation size로 최적화 하는 법)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,9 +20,34 @@ public class JpaMain {
 
         try {
 
+            Member member1 = new Member();
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            Member member4 = new Member();
+            member4.setUsername("D");
+            System.out.println("==================");
+
+            //allocationSize가 50인데, 처음 호출하면 SEQ값이 1이라, 한 번 더 next value를 호출하므로 처음 두 번 시퀀스 next value 호출
+                                // 실제 SEQ 값  | SEQ테이블 값
+            em.persist(member1); //     1       |       1
+            em.persist(member2); //     2       |      51
+            em.persist(member3); //     3       |      51
+            em.persist(member4); //     4       |      51       //51개까지 메모리에서 시퀀스를 세다가, 다 쓰면 50개 더 호출
+
+            System.out.println("member1.getId() = " + member1.getId());
+            System.out.println("member2.getId() = " + member2.getId());
+            System.out.println("member3.getId() = " + member3.getId());
+            System.out.println("member4.getId() = " + member4.getId());
+
+            System.out.println("====================");
 
 
-            System.out.println("===========");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,44 +1,60 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 /*
-* 기본 생성자 필수
-* final클래스, enum, interface, inner 클래스 사용 불가!
-* 저장할 필드에 final 사용 X
-* */
+ * 권장하는 식별자 전략
+ * 기본 키 제약조건 : null 아님, 유일한 값, 변하면 안된다.
+ * 미래까지 이 조건을 만족하는 자연키(실제 비즈니스 정보)는 찾기 어렵다. 대리키(UUID,시퀀스 오브젝트,Auto Increment)를 사용하자.
+ * 가령, 주민번호도 기본 키로 적절하지 않다.
+ * 권장 : Long형 + 대체키 + 키 생성전략 사용
+ * */
+/*
+ * 기본 생성자 필수
+ * final클래스, enum, interface, inner 클래스 사용 불가!
+ * 저장할 필드에 final 사용 X
+ * */
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    //javax.persistence의 @Id 어노테이션으로 JPA에게 PK를 알려줌.
+    /*
+     * IDENTITY 전략에서는, em.persist() 호출 시점에 바로 insert 쿼리가 날아간다.
+     * 영속성 컨텍스트에는 key값으로 id값을 들고 있어야 하는데, 전략을 db에 위임했으므로,
+     * 쿼리를 보내지 않고서는 id를 알 수 없으므로.
+     * 따로 select 쿼리는 안날아간다. JDBC 드라이버에 insert 시점에 리턴받는 식으로 짜여져 있으므로, JPA는 그것을 활용
+     * 여러번 네트워크를 타긴 하지만, 한 트랜잭션 안에서 이루어지므로 큰 성능 손실은 없다.
+     * 결국 시퀀스를 지원하지 않는 DB는 IDENTITY가 최선.
+     * */
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+
     private Long id;
 
-    // DDL 생성기능 : DDL을 자동 생성할 때만 사용되고 JPA의 실행 로직에는 영향을 주지 않는다
-    @Column(name ="name",nullable = true, columnDefinition = "varchar(100) default 'ddd'") // columnDefinition : 데이터베이스 컬럼 정보를 직접 줄 수 있다.
+
+    @Column(name = "name", nullable = false)
     private String username;
 
-    @Column(unique =true, length = 10) // unique는 제약조건 이름을 설정할 수 없어서 실제로 쓰지 않고, @Table(uniqueConstraints = )으로 설정한다
-    private Integer age;
+    public Member() {
+    }  //@Entity를 붙인 엔티티객체는 기본 생성자가 필요함. (리플렉션 같은 기술을 쓰기 위해)
 
-    @Enumerated(EnumType.STRING)  // EnumType은 기본값이 ORDINAL(순서를 DB에 저장하는 방식)이므로, 반드시 STRING TYPE으로 지정해준다.
-    private RoleType roleType;
+    public Long getId() {
+        return id;
+    }
 
-    // 날짜 타입 매핑. LocalDate, LocalDateTime 타입을 쓰면  이 설정이 필요없다. (최신 하이버네이트 지원)
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
+    public String getUsername() {
+        return username;
+    }
 
-    @Lob //Large Object를 사용하려면 @Lob을 사용 (CLOB, BLOB ...) // 매핑하는 필드 타입이 문자면 CLOB, 나머지는 BLOB로 매핑
-    private String description;
-
-    @Transient // 이 필드를 컬럼에 매핑하지 않음. DB에 저장 X, 조회X. 메모리 상에서 임시로 어떤 값을 보관하고 싶을 때 사용
-    private int temp;
-
-    public Member() {}  //@Entity를 붙인 엔티티객체는 기본 생성자가 필요함. (리플렉션 같은 기술을 쓰기 위해)
-
-
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
